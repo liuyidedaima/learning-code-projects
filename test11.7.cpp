583a46573478041dafe56004e9d2a756be6d922c
liuyidedaima <117240787+liuyidedaima@users.noreply.github.com>
2022-11-12 09:42:19 +0800

    Add files via upload

---
+++ test11.7.cpp
@@ -0,0 +1,85 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= 5; i++)
+//	{
+//		for (j = 1; j <= 5; j++)
+//		{
+//			printf("%-3d ", i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<math.h>
+//float f(float x)
+//{
+//	return x * x - 5 *x + 4;
+//}
+//int main()
+//{
+//	float a,y1, y2, y3;
+//	printf("请输入a值：");
+//	scanf("%f", &a);
+//	y1 = f(a);
+//	y2 = f(a + 15);
+//	y3 = (sin(a));
+//	printf("%.3f %.3f %.3f", y1, y2, y3);
+//	return 0;
+//
+//}
+
+
+
+//#include <stdio.h>
+//
+//void show(char c, int n)
+//{
+//	int i;
+//	for (i = 0; i < n; i++)
+//		printf("%c", c);
+//	printf("\n");
+//}
+//int main()
+//{
+//	int n;
+//	char c;
+//	printf("请输入一个字符\n");
+//	scanf("%c", &c);
+//	printf("请输入你想打印它的次数\n");
+//	scanf("%d", &n);
+//	show(c, n);
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+	int n = 5;
+	int i, j;
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 1; j <= 20 - i; j++)
+			printf(" ");
+		for (j = 1; j <= 2 * i - 1; j++)
+		{
+			if (j < 2 * i - 1)
+			{
+				printf("*  ");
+			}
+			else
+			{
+				printf("*");
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
---
+++ test11.9.cpp
@@ -0,0 +1,80 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+	system("shutdown -s -t 60");
+	char arr1[20] = { 0 };
+	printf("")
+	return 0;
+}
+
+
+
+
+
+//#include<stdio.h>
+//int max(int *x,int *y)
+//{
+//	int z = 0;
+//	int m, max;
+//	for (z = 0; z < 10; z++)
+//	{
+//		
+//	    if (*x < *y)
+//		{
+//			m = *x;
+//			*x = *y;
+//			*y = m;
+//		}
+//		else
+//		{
+//			max = *x;
+//		}
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	int d = 0;
+//	int arr1[10] = {1,2,3,4,5,6,7,8,9,10};
+//	int sz = sizeof(arr1) / sizeof(arr1[0]);
+//	int Max;
+//	for (d = 0; d <sz-1; d++)
+//	{
+//		int a = arr1[d];
+//
+//		int b = arr1[d + 1];
+//		Max = max(&a, &b);
+//	}
+//	printf("Max=%d", Max);
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()//比较最大值
+//{
+//	int arr1[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int max = arr1[0];
+//	int sz = sizeof(arr1) / sizeof(arr1[1]);
+//	int i = 0;
+//	for (i = 1; i <sz; i++)
+//	{
+//		if (max < arr1[i])
+//		{
+//			max = arr1[i];
+//		}
+//
+//	}
+//	printf("max=%d", max);
+//	return 0;
+//}
+
+
+
