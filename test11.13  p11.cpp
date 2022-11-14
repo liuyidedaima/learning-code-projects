cc146a5a852567c1c73a54ab7d511dda2f2f1b95
liuyidedaima <117240787+liuyidedaima@users.noreply.github.com>
2022-11-14 21:38:42 +0800

    Add files via upload

---
+++ test11.13  p11.cpp
@@ -0,0 +1,120 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+// //函数的递归
+//#include<stdio.h>
+//void print(int x)//递归打印
+//{
+//	if (x > 9)
+//	{
+//		print(x / 10);//依次求个位
+//	}
+//	printf("%d ", x % 10);
+//}
+//int main()
+//{
+//	int i = 0;
+//	scanf("%d", &i);
+//	print(i);
+//	return 0;
+//}
+
+
+////用递归解释strlen
+//#include<stdio.h>
+//
+//int my_strlen(char *arr)
+//{
+//	if (*arr != '\0')
+//	{
+//		return 1 + my_strlen(arr + 1);//分解首个字母
+//	}
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	char arr[] = "hello world";
+//		int i = my_strlen(arr);
+//		printf("%d", i);
+//	return 0;
+//}
+
+
+////用递归求阶乘
+//#include<stdio.h>
+//
+//int fal(int m)
+//{
+//	if (m < 2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return m * fal(m - 1);
+//	}
+//}
+//int main()
+//{
+//	int ret = 0;
+//	int i = 0;
+//	scanf("%d", &i);
+//	ret = fal(i);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+
+////用递归求斐波那契数列
+//#include<stdio.h>
+//
+//int fal(int n)
+//{
+//	if (n < 3)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return fal(n - 1) + fal(n - 2);
+//	}
+//}
+//int main()
+//{
+//	int ret = 0;
+//	int i = 0;
+// printf("请输入想求第几个斐波那契数");
+//	scanf("%d", &i);
+//	ret = fal(i);
+// printf("%d",ret);
+//	return 0;
+//}
+
+
+//高效求斐波那契数
+//#include<stdio.h>
+//
+//int fal(int n)
+//{
+//	int a = 1, b = 1, c = 1;
+//	while(n >= 3)//用三个变量反复变换求斐波那契数
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;//决定执行交换次数
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int ret = 0;
+//	int i = 0;
+//	printf("请输入想求第几个斐波那契数");
+//	scanf("%d", &i);
+//	ret = fal(i);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+
---
+++ test11.14.cpp
@@ -0,0 +1,73 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+
+//#include<stdio.h>//计算阶乘
+//
+//int fal(int n)
+//{
+//	if (n == 1)
+//		return 1;
+//	else
+//	return n * fal(n - 1);
+//}
+//int main()
+//{
+//	int i = 0;
+//	int ret = 0;
+//	printf("请输入你想计算的阶乘:");
+//	scanf("%d", &i);
+//	ret = fal(i);
+//	printf("结果是:%d",ret);
+//	return 0;
+//}
+
+
+//#include<stdio.h>//猴子摘桃问题
+//
+//int total_peach(int n)
+//{
+//	return (n + 1) * 2;
+//}
+//
+//int main()
+//{
+//	int peach = 0;
+//	int rest = 1;
+//	int day = 9;
+//	for (day = 1; day < 10; day++)
+//	{
+//		peach = total_peach(rest);
+//			rest = peach;
+//	}
+//	printf("第一天摘了%d个桃子", rest);
+//	return 0;
+//}
+
+
+//#include<stdio.h>水仙花问题
+//
+//int nube(int n)
+//{
+//	return n * n * n;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int a, b, c;
+//	for (i = 100; i <= 999; i++)
+//	{
+//		a = i / 100;
+//		b =( i / 10) % 10;
+//		c = i % 10;
+//		if (i == nube(a) + nube(b) + nube(c))
+//		{
+//			printf("水仙数是%d\n", i);
+//		}
+//		else
+//		{
+//			continue;
+//		}
+//
+//	}
+//	return 0;
+//}
